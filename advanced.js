7d075509286b5d12e7bdfd17b5a5f9e303bb4749
sohel5G <contactsohelrana@gmail.com>
2023-08-26 20:42:11 +0600

    first commit

---
+++ advanced.js
@@ -0,0 +1,78 @@
+const arr = [3, 6, 7, 2, 2, 99, 3, 5, 32];
+
+// const result = arr.map( x => x + 10 );  // map return kore
+// console.log( result );
+
+// const result = arr.forEach((x) => x + 10);  // forEach return kore na
+// console.log(result);
+
+// const result = arr.filter((x) => x > 10); // filter check all and get the match condition item
+// console.log(result);
+
+// const result = arr.find((x) => x > 10); // just prothom ta return kore
+// console.log(result);
+
+// const result = arr.reduce( (prev, cur) => prev + cur, 0 );
+// console.log(result);
+
+
+
+
+
+
+
+// const arr1 = [
+//   { id: 1, name: "lenovo", price: 34000 },
+//   { id: 2, name: "hp", price: 44000 },
+//   { id: 3, name: "dell", price: 54000 },
+//   { id: 4, name: "apple", price: 154000 },
+// ];
+// // console.log(arr1.map((produt) => produt.price));
+// // console.log(arr1.filter((price) => price.price > 40000));
+// console.log(arr1.reduce((prev, cur) => prev + cur.price, 0));
+
+
+
+
+
+
+
+
+
+// // JavaScript Class
+
+// class classNme {
+//   constructor(name, subject) {
+//     this.name = name;
+//     this.subject = subject;
+//   }
+//   lecture() {
+//     console.log('Sir is teaching Math');
+//   }
+// }
+// const papon = new classNme('Papon Sir', 'Math');
+// console.log(papon);
+// const roshid = new classNme('Roshid sir', 'English');
+// console.log(roshid);
+
+
+
+// class Vehicle {
+//   constructor(name, price, maidIn) {
+//     this.name = name;
+//     this.price = price;
+//     this.maidIn = maidIn;
+//   }
+//   move() {
+//     console.log('Gari sola sol kore');
+//   }
+// }
+// class Bus extends Vehicle {
+//   constructor(name, price, seat, ticketPrice) {
+//     super(name, price);
+//     this.seat = seat;
+//     this.ticketPrice = ticketPrice;
+//   }
+// }
+
+
